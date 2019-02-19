c635b52a0614a0b7a57cdef63bed12c5ecc3dcae
myGitUser13 <vanbeethoven01@yahoo.com>
2019-02-19 13:13:57 +0300

    setupProxy not working on conditional check of env,must explicitly require http-proxy-middleware

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,14 +1,5 @@
-
-let proxy;
-const config = require('./config');
-if(config.env === 'prod'){
- console.log(config.env)
- proxy = require('http-proxy-middleware');
-}
-
+let proxy = require('http-proxy-middleware');
 //if multipart url does not work check this file
 module.exports = function(app){
- if(proxy){
   app.use(proxy('/apiv1',{target:'http://localhost:1234/'}))
- }
 }
