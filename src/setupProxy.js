0fd08bab302cf21cfc2486a58ffcae862cb30b76
myGitUser13 <vanbeethoven01@yahoo.com>
2019-02-20 13:06:04 +0300

    Modified to work on Prod

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,6 @@
-let config = require('./config');
-let proxy = config.env === 'prod'? require('http-proxy-middleware'): null;
-
+let proxy = require('http-proxy-middleware');
 
 //if multipart url does not work check this file
-module.exports = config.env === 'prod' ? function(app){
+module.exports = function(app){
   app.use(proxy('/apiv1',{target:'http://localhost:1234/'}))
-}:()=>{}
+}
