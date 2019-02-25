0c1776a901e2d86bb0810624c9a8d35619b52b50
myGitUser13 <vanbeethoven01@yahoo.com>
2019-02-25 11:23:33 +0300

    Added artifact emitter,entity,permission

--- src/features/entities/Entity.js
+++ src/features/entities/Entity.js
@@ -1,28 +1,61 @@
-class Entity{
+import ArtifactEmitter from "../ArtifactEmitter";
+
+class Entity extends ArtifactEmitter{
  constructor(object){
+  super();
   if(typeof object !== 'object') throw new Error(`@Entity : ${object} is not of type object `);
   Object.assign(this,object);
  }
 
  async save(){
+  let artifact;
   if(!this._id){
    try {
-     let res = await this.constructor.update(this); 
-     return new this.constructor(res.data.data.entity);
+     artifact = await this.constructor.save(this); 
+     if(artifact.status === 'ok'){
+      Object.assign(this,artifact.data.entity);
+      this.emit(artifact);
+      return this._id;
+     }
+      this.emit(artifact);
+      return false;
    } catch (error) {
-     console.log(error);
-     emit('entity-action',{type:"error",text:"Error on Save"});
+      console.log(error);
+     this.emit(artifact);
+     return false;
    }
   }
-  return await this.constructor.save(this);
+
+  try {
+    artifact = await this.constructor.update(this);  
+    if(artifact.status === 'ok'){
+      Object.assign(this,artifact.data.entity);
+      this.emit(artifact);
+      return true;
+    }
+    this.emit(artifact);
+    return false;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+  
  }
 
- async remove(){
+ async delete(){
+  let artifact;
   try {
-   return await this.constructor.delete(this);
+   artifact = await this.constructor.delete(this);
+   if(artifact.status === 'ok'){
+     Object.getOwnPropertyNames(this).forEach(p=>{
+       delete this[p];
+     });
+     return true;
+   }
+   this.emit(artifact);
+   return false;
   } catch (error) {
    console.log(error);
-   emit('entity-action',{type:"error",text:"Error on Save"});
   }
   
  }
--- src/features/entities/Permissions.js
+++ src/features/entities/Permissions.js
@@ -1,13 +1,14 @@
 
+import Entity from './Entity';
+import {permission_browse as browse} from '../requesters';
+import {permission_add as save} from '../requesters';
+import {permission_edit as update} from '../requesters';
+import {permission_delete as del} from '../requesters';
 
-class Permission extends Entity{
- constructor(object){
-  super(object);
- }
-}
-
-
-Permission.save = {permission_add} = require('../requesters');
-Permission.update = {permission_edit} = require('../requesters');
+class Permission extends Entity{}
+Permission.browse = browse;
+Permission.save = save;
+Permission.update = update;
+Permission.delete = del;
 
 export default Permission;
