429ab18b42d7317f414d4c8d9c7d95af774520c9
myGitUser13 <vanbeethoven01@yahoo.com>
2019-04-25 00:12:43 +0300

    Cleaned up index

--- src/index.js
+++ src/index.js
@@ -4,9 +4,5 @@ import './index.css';
 import axios from './axios';
 import App from './App';
 
-const Test = ()=>{
- axios.post('/apiv1/login',{username:"htu00001",password:"eGPK1P2G#3"}).then(r=>console.log(r));
- return <div>X</div>
-}
-ReactDOM.render(<Test />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
