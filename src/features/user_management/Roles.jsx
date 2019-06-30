062fa15b4334a75f027ae7f2f6fdce8912cecf1f
myGitUser13 <vanbeethoven01@yahoo.com>
2019-06-30 13:54:49 +0300

    Modified Role to use it's own bread

--- src/features/user_management/Roles.jsx
+++ src/features/user_management/Roles.jsx
@@ -1,6 +1,10 @@
 import React, { useState,useEffect,useReducer } from 'react';
+import {Route,Switch} from 'react-router-dom';
 import featureGroups from '../featureGroups';
 import EBrowser from '../../comps/EBread/EBrowser';
+import EAdder from '../../comps/EBread/EAdder';
+import EReader from '../../comps/EBread/EReader';
+import EEditor from '../../comps/EBread/EEditor';
 import RoleUISchema from '../uischemas/Role';
 
 
@@ -10,7 +14,7 @@ import {
 
 import { subscribe } from '../../event';
 
-function Roles({user}){
+function Roles({user,history}){
 
  const [roles,setRoles] = useState([]);
 
@@ -18,7 +22,7 @@ function Roles({user}){
   switch(eventResult.source){
    case 'role_browse': {
     if(eventResult.type === 'artifact'){
-     console.log(eventResult.artifact);
+     console.log(eventResult.artifact.entity);
      setRoles(eventResult.artifact.entity);
     }
    }
@@ -32,14 +36,52 @@ function Roles({user}){
   return unsubscribe;
  },[]);
 
- return(
-  <EBrowser 
-   UISchema={RoleUISchema}
-   entities={roles}
-   onDelete={(entity)=>console.log('Deleting ',entity)}
-   onReady = {(entity)=>console.log('Reading ',entity)}
+ const ADDER_PATH = '/roles/add';
+ const EDITOR_PATH = '/roles/:identifier/edit';
+ const READER_PATH = '/roles/:identitier';
+
+ const onRead = entity => {
+  console.log(EDITOR_PATH.replace(":identifier",entity["_id"]));
+  if(user.hasPermission('role_edit')){
+   history.push(`${EDITOR_PATH.replace(":identifier",entity['_id'])}`,{entity});
+   return;
+  }
+  history.push(`${READER_PATH.replace(":identifier",entity['_id'])}`,{entity});
+ }
+
+ const reader = mlh => 
+  <EReader 
+   identifier="_id"
+   UISchema={RoleUISchema} 
+   entity={mlh.location.state.entity} 
+   editorPath={EDITOR_PATH} 
+   onDelete={()=>{}} 
   />
- 
+
+  const editor = mlh => 
+  <EEditor 
+   identifier="_id"
+   UISchema={RoleUISchema} 
+   entity={mlh.location.state.entity} 
+   onSave={()=>{}} onDelete={()=>{}} 
+  /> 
+
+ const adder = mlh => <EAdder UISchema={RoleUISchema} onSave={()=>{}} /> 
+
+ return(
+  <React.Fragment>
+   <Switch>
+     <Route path={ADDER_PATH} exact render={ adder }/> 
+     <Route path={EDITOR_PATH} exact render={ editor }/> 
+     <Route path={READER_PATH} exact render={ reader }/>    
+   </Switch>
+   <EBrowser 
+    UISchema={RoleUISchema}
+    entities={roles}
+    onDelete={(entity)=>console.log('Deleting ',entity)}
+    onRead = {onRead}
+   />
+  </React.Fragment>
  )
 }
 
