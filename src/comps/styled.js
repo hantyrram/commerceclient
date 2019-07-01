497eadba5c35f74c57e9acc5e537007d1200d05c
myGitUser13 <vanbeethoven01@yahoo.com>
2019-07-01 13:18:02 +0300

    Define styled reusable elements/components here

---
+++ src/comps/styled.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+/**
+ * @namespace styled
+ * @description This can be used as the main wrapper of each exported component.
+ */
+export const ComponentContainer = styled.div`
+ margin: .2em;
+ border: ${boxed => boxed ? '1px solid #dcd5d5': 'none'}
+`;
+
