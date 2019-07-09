65e35b94d9f8da27eae32aa5d60c5d5c1f444902
myGitUser13 <vanbeethoven01@yahoo.com>
2019-07-09 10:35:33 +0300

    merge with dev

--- src/features/index.js
+++ src/features/index.js
@@ -1,29 +1,10 @@
 
 
 import Permissions from './user_management/Permissions';
-<<<<<<< HEAD
-import Roles from './user_management/Roles';
-import UserBrowse from './UserBrowse';
-import ProductBrowse from './ProductBrowse';
-import OrderBrowse from './OrderBrowse';
-=======
->>>>>>> dev
 import Users from './user_management/Users';
 import Roles from './user_management/Roles';
 export default [
-<<<<<<< HEAD
-  // PermissionCreate,
-  // PermissionBrowse,
-  // PermissionRead,
   Users,
   Permissions,
   Roles
-  // UserBrowse,
-  // ProductBrowse,
-  // OrderBrowse
-=======
-  Users,
-  Permissions,
-  Roles
->>>>>>> dev
 ]
