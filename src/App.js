89b68f4a1831f9004cdbed2d16ee36cd16ba07b1
hantyrram <hantyr@Hantyr.localdomain>
2020-05-15 20:33:33 +0300

    Puts Redirect directly on the Route render prop instead of letting the AdminPage handle the redirect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
    BrowserRouter as Router, 
    Redirect,
    Route,
    Switch} from 'react-router-dom';
-   
+import useAppState from 'appstore/useAppState';
 import LoginPage from 'pages/Login';
 import AdminPage from 'pages/Admin';
-import useAppState from 'appstore/useAppState';
-import useApiRequest from 'api/useApiRequest'
+
 
 function App(props){
+
+   const { identity } = useAppState().getAppState();
 
    return(
       <Router>
          <Switch>
-            <Route path="/auth/login" component={LoginPage} />
-            <Route path="/" component={AdminPage} />
+            <Route exact path="/auth/login" component={LoginPage} />
+            <Route path="/" 
+               render = { 
+                  (props) => {
+                     if(!identity){
+                        return <Redirect to="/auth/login" />
+                     }
+                     return (
+                        <AdminPage {...props} />
+                     )
+                  } 
+               } />
          </Switch>
       </Router>
    )
