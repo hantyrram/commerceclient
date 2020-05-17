11035ba331688d156d94cfc9080e0a3351068c86
hantyrram <hantyr@Hantyr.localdomain>
2020-05-17 22:30:00 +0300

    Changed proxy path from /apiv1 to /cbo/apiv1

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@
 let proxy = require('http-proxy-middleware');
 //if multipart url does not work check this file
 module.exports = function(app){
-  app.use(proxy('/apiv1',{
+  app.use(proxy('/cbo/apiv1',{
    target:'http://localhost:8080/',
    logLevel:"debug"
   }))
